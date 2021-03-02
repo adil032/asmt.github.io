a79e881781dfc777d12077d1c0656031d46d75cb
adil032 <66691957+adil032@users.noreply.github.com>
2021-03-02 15:27:35 +0530

    asmt

---
+++ asmt.js
@@ -0,0 +1,7 @@
+console.log("this is a project");
+
+
+
+const mouseTarget = document.getElementById('a');
+mouseTarget.addEventListener('mouseleave', function(){
+mouseTarget.style.border = '1px solid red'; });
